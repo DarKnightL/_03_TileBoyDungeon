15f43bc49b3b84b91f51c071029d6b128b52d51f
simonhomoludens <liusihui@outlook.com>
2019-02-15 15:22:00 +0800

    6th Version_EnemySemiFinished

--- Assets/MossGiant.cs
+++ Assets/MossGiant.cs
@@ -4,52 +4,5 @@ using UnityEngine;
 
 public class MossGiant : Enemy {
 
-    Vector3 currentTarget;
-    Animator mossAnimator;
-    SpriteRenderer mossSprite;
-
-
-    private void Start()
-    {
-        mossAnimator = GetComponentInChildren<Animator>();
-        mossSprite = GetComponentInChildren<SpriteRenderer>();
-    }
-
-    public override void Update()
-    {
-        if (mossAnimator.GetCurrentAnimatorStateInfo(0).IsName("MossGiantIdle"))
-        {
-            return;
-        }
-
-        Movement();
-    }
-
-
-
-    void Movement() {
-        if (currentTarget==pointA.position)
-        {
-            mossSprite.flipX = true;
-        }
-        else if (currentTarget==pointB.position)
-        {
-            mossSprite.flipX = false;
-        }
-      
-
-
-        if (transform.position==pointA.position)
-        {
-            currentTarget = pointB.position;
-            mossAnimator.SetTrigger("Idle");
-        }
-        else if (transform.position==pointB.position)
-        {
-            currentTarget = pointA.position;
-            mossAnimator.SetTrigger("Idle");
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
-    }
+    
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,8 +15,65 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     protected Transform pointB;
 
-    public abstract void Update();
+    protected SpriteRenderer enemySprite;
+    protected Animator enemyAnimator;
 
-    public virtual void Attack() { }
+    private Vector3 currentTarget;
 
+
+
+    
+
+
+    public virtual void Init() {
+        enemySprite = GetComponentInChildren<SpriteRenderer>();
+        enemyAnimator = GetComponentInChildren<Animator>();
+    }
+
+
+    private void Start()
+    {
+        Init();
+    }
+
+
+    public virtual void Update()
+    {
+        if (enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        Movement();
+    }
+
+
+    public virtual void Movement() {
+
+
+        if (currentTarget == pointA.position)
+        {
+            enemySprite.flipX = true;
+        }
+        else if (currentTarget == pointB.position)
+        {
+            enemySprite.flipX = false;
+        }
+
+
+
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            enemyAnimator.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            enemyAnimator.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
 }
+
+
